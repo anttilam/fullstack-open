de9ccc33b0d7939b77add424960ec73cfef3de60
anders <antti.lmsa@gmail.com>
2025-01-01 14:22:20 +0200

    Tehtävät 2.16 - 2.19

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import apiServices from './services/apiServices'
+import './index.css'
 
+const Notification = ({ message, isError }) => {
+
+  if (!message) return null
+
+  return (
+    <div className={`notification ${isError ? 'error' : 'success'}`}>
+      {message}
+    </div>
+  )
+
+}
 
 const Phonebook = ({ setSearchFilter }) => {
   return (
@@ -9,12 +22,12 @@ const Phonebook = ({ setSearchFilter }) => {
   )
 }
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, remove }) => {
 
   return (
     <>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number} <button type="button" onClick={() => remove(person)}>delete</button></div>)}
     </>
   )
 }
@@ -30,7 +43,6 @@ const PersonsForm = ({ setNewPerson, handleSubmit, newPerson }) => {
         <div>
           number: <input value={newPerson.number} onChange={(e => setNewPerson({ ...newPerson, number: e.target.value }))} />
         </div>
-
         <div>
           <button type="submit" onClick={(e) => handleSubmit(e)}>add</button>
         </div>
@@ -40,37 +52,87 @@ const PersonsForm = ({ setNewPerson, handleSubmit, newPerson }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState()
   const [newPerson, setNewPerson] = useState({ name: '', number: '' })
-  const [searchFilter, setSearchFilter] = useState('')
+  const [searchFilter, setSearchFilter] = useState('');
+  const [noteMsg, setNoteMsg] = useState(null)
+  const [error, setError] = useState(false)
+
+  const getPersons = () => {
+    apiServices.getAll().then(persons => {
+      setPersons(persons)
+    })
+  }
+
+  useEffect(() => {
+    getPersons()
+  }, [])
+
+  const handleDelete = async (person) => {
+
+    if (confirm(`Delete ${person.name}?`)) {
+      try {
+        await apiServices.remove(person.id)
+        await getPersons()
+      } catch (error) {
+        setError(true)
+        setNoteMsg(`Information of ${person.name} has already been removed from server`)
+        setTimeout(() => {
+          setNoteMsg(null)
+          setError(false)
+        }, 5000)
+      }
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    const names = persons.map(person => person.name)
+    const names = persons && persons.map(person => person.name)
 
+    //handle update person
     if (names.some((person) => person.toLocaleLowerCase() === newPerson.name.toLocaleLowerCase())) {
-      alert(`${newPerson.name} is already added to phonebook`)
+      const updatedPersonId = persons.find(person => person.name.toLocaleLowerCase() === newPerson.name.toLocaleLowerCase()).id
+      const updatedPerson = { name: newPerson.name, number: newPerson.number }
+
+      if (confirm(`${newPerson.name} is already added to phonebook. Replace old number with a new one?`)) {
+
+        apiServices.update(updatedPersonId, updatedPerson).then(updatedPersonRes => {
+          setPersons(persons.map(person => person.id !== updatedPersonRes.id ? person : updatedPersonRes))
+        }).catch(error => {
+          setError(true)
+          setNoteMsg(`Information of ${newPerson.name} has already been removed from server`)
+          setTimeout(() => {
+            setNoteMsg(null)
+            setError(false)
+          }, 5000)
+        })
+      }
+
     } else {
       setNewPerson({ name: '', number: '' })
-      setPersons([...persons, newPerson])
+      apiServices.create(newPerson).then(newPerson => setPersons([...persons, newPerson]))
+      setNoteMsg(`Added ${newPerson.name}`)
+      setTimeout(() => {
+        setNoteMsg(null)
+      }, 5000)
     }
   }
 
-  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchFilter))
+  const filteredPersons = persons && persons.filter(person => person.name.toLowerCase().includes(searchFilter))
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Phonebook setSearchFilter={setSearchFilter} />
-      <div style={{ marginBottom: '30px' }}></div>
-      <PersonsForm setNewPerson={setNewPerson} handleSubmit={handleSubmit} newPerson={newPerson} />
-      <Persons persons={filteredPersons} />
+      {persons && (
+        <>
+          <h2>Phonebook</h2>
+          <Notification isError={error} message={noteMsg} />
+          <Phonebook setSearchFilter={setSearchFilter} />
+          <div style={{ marginBottom: '30px' }}></div>
+          <PersonsForm setNewPerson={setNewPerson} handleSubmit={handleSubmit} newPerson={newPerson} />
+          <Persons persons={filteredPersons} remove={handleDelete} />
+        </>
+      )}
     </div>
   )
 
